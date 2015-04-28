176df23778b869ba3a6b880af1a394ae5eda6c45
Lukasz Milik <lmilik@infusion.com>
2015-04-28 12:04:58 +0200

    Major changes to site

--- wp-content/themes/flyboard/header.php
+++ wp-content/themes/flyboard/header.php
@@ -22,13 +22,11 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
  <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta property="og:url" content="http://www.cfkrakow.com/" />
-<meta property="og:image" content="http://cfkrakow.nazwa.pl/www/wp-content/themes/cf-krakow/inc/logo.jpg" />
+<meta property="og:url" content="http://www.superflykrakow.pl" />
 <meta property="og:title" content="<?php wp_title( '|', true, 'right' ); ?>" />
-<meta property="og:description" content="Akademia treningu siłowego i kondycyjnegoprowadzona w Krakowie przez licencjonowanych instruktorów." />
+<meta property="og:description" content="Loty flyboard kraków" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
-	
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/favicon.ico" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/favicon.ico" />	
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="image_src" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/logo.jpg" />
@@ -40,6 +38,15 @@
 <?php wp_head(); ?>
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/icons.css" type="text/css" media="all"/>
 <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<script>
+var cookie_arr = cookie_arr || [];
+cookie_arr.push('white'); // Layout
+(function() { var cookie = document.createElement('script');
+cookie.type = 'text/javascript'; cookie.src = '//cookiesinfo.com/cookie_v2.js';
+var cookie_node = document.getElementsByTagName('script')[0];
+cookie_node.parentNode.insertBefore(cookie,cookie_node);
+})();
+</script>
 <script type="text/javascript" src="/wp-content/plugins/easy-contact-forms/easy-contact-forms-forms.1.4.9.js"></script>
 <script>(function() {
   var _fbq = window._fbq || (window._fbq = []);
@@ -71,10 +78,9 @@ window._fbq.push(['track', 'PixelInitialized', {}]);
 
 
 	<header id="masthead" class="site-header" role="banner">
-				<div class="logo-wrapper">
-				
-				
-				</div>
+				<a href="http://www.google.pl" title="logo">
+					<img class="logo-wrapper" src="<?php echo get_bloginfo('template_directory'); ?>/inc/logo.png" alt="" />
+				</a>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
